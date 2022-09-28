6a9ac0b2bd3b8ca79446ae37fac65436c75c3f5e
EricJamesCrow <EricCrow@pm.me>
2022-09-28 10:56:24 -0700

    update Home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -22,6 +22,29 @@ export default function Home() {
         ))}
         </div>
     </div>
+    <div className="special-deals">
+      <div className="special-deals-label">Special Deals</div>
+      <div className="special-deals-products">
+        {products.slice(0, 3).map(product => (
+          <Product/>
+        ))}
+      </div>
+    </div>
+    <div className="about">
+      <div className="about-label">About</div>
+          <div className="about-text">
+          Lorem ipsum dolor sit amet, putent vituperatoribus te nam, ut summo atomorum qualisque vis. Ex eius scriptorem vis, nec rebum propriae voluptatibus ex. Ei cum munere tempor noluisse, cum eu corpora adversarium.
+          </div>
+    </div>
+    <div className="newsletter">
+          <div className="newsletter-label">Subscribe to our Newsletter</div>
+          <div className="newsletter-text">
+          Et vim falli omittam, mei aeterno mandamus vulputate ut. Sit quidam legimus nominavi et. Augue dissentias pro in, te indoctum conclusionemque sea, tation honestatis te vix. No has illud mucius mandamus, assum scribentur quo ad, eum an ignota dissentias.
+          </div>
+    </div>
+    <div className="copyright">
+      <div>© Mantra Seeds 2022</div>
+    </div>
     </>
   )
 }
