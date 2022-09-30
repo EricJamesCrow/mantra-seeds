054a76d716a6d73e21fbc73a9e4daff6aa02ec05
EricJamesCrow <EricCrow@pm.me>
2022-09-29 17:36:23 -0700

    sidebar works

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -17,15 +17,32 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 // components
 import Product from "../../components/Product"
 
+// react
+import { useState, useEffect } from 'react';
+
 export default function Store() {
     const chakras = [{"id": "1", "image": Crown, "text": "Crown", "textColor": "#8f009c"}, {"id": "2", "image": ThirdEye, "text": "ThirdEye", "textColor": "#00489c"},
     {"id": "3", "image": Throat, "text": "Throat", "textColor": "#00b5db"}, {"id": "4", "image": Heart, "text": "Heart", "textColor": "#8cbf00"}, {"id": "5", "image": Solar, "text": "Solar", "textColor": "#e3c101"},
     {"id": "6", "image": Sacral, "text": "Sacral", "textColor": "#ff9200"}, {"id": "7", "image": Root, "text": "Root", "textColor": "#e60000"}]
     const products = ["weed", "weed", "weed", "weed", "weed", "weed", "weed", "weed"]
+    const [stickyClass, setStickyClass] = useState("store-sidebar");
 
+    useEffect(() => {
+      window.addEventListener('scroll', whatsWindowHeight);
+  
+      return () => {
+        window.removeEventListener('scroll', whatsWindowHeight);
+      };
+    }, []);
+
+    const whatsWindowHeight = () => {
+      let windowHeight = window.scrollY;
+      windowHeight > 170 ? setStickyClass('store-sidebar-sticky') : setStickyClass("store-sidebar");
+      };
+// style={{ paddingTop: stickyClass[0], position: stickyClass[1] }}
   return (
     <div className="store-container">
-      <div className="store-sidebar">
+      <div className={stickyClass} style={{ paddingTop: stickyClass[0], position: stickyClass[1] }}>
         <div className="filter-products-container">
           <div className="filter-products-label">Filter Products</div>
         </div>
