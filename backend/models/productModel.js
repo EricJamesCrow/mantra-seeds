b50b8a76c2768154e4a9236febe23f01bff70ac2
EricJamesCrow <EricCrow@pm.me>
2022-10-11 19:20:18 -0700

    added backend

---
+++ backend/models/productModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    chakra: {
+        type: String,
+        required: true
+    },
+    strain: {
+        type: String,
+        required: true
+    },
+    thc: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true})
+
+module.exports = mongoose.model('Product', productSchema)
