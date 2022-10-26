0520acaaa3ee370e4525ec9cfe8d2d0ee019a9d8
EricJamesCrow <EricCrow@pm.me>
2022-10-26 16:44:49 -0700

    creating profile page

--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -2,6 +2,14 @@ import { useAuthContext } from "../../hooks/useAuthContext"
 import { Link, useNavigate } from 'react-router-dom'
 import { useLogout } from '../../hooks/useLogout'
 
+// styles
+import "./Profile.css"
+
+// images
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass, faShoppingCart, faUser, faList } from '@fortawesome/free-solid-svg-icons'
+
+
 export default function Profile() {
   const { user } = useAuthContext()
   let navigate = useNavigate()
@@ -13,12 +21,41 @@ export default function Profile() {
     logout()
   }
 
+  String.prototype.toProperCase = function () {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
+
+  const obj = {
+    "orders": faMagnifyingGlass,
+    "cart": faShoppingCart,
+    "account": faUser,
+    "list": faList
+  }
+
   return (
     <div>
       {user && <>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', justifyContent: 'center', alignItems: 'center', paddingTop: "200px", paddingBottom: "280px"}}>
-      <span>{user.email}</span>
+      <div className="profile-header">
+      <span>Hello, {user.email}</span>
       <button onClick={handleClick}>Logout</button>
+      </div>
+      <div className="profile-buttons">
+      {Object.entries(obj)
+        .map( ([key, value]) => <Link to={`/${key}`}>Your {key.toProperCase()}
+            <FontAwesomeIcon
+            icon={value}
+            />
+            </Link>)}
+      </div>
+      <div className="quick-account-access">
+        <div className="div-span"></div>
+        <div className="quick-account-access-text">Your Orders</div>
+        <div className="div-span"></div>
+        <div className="quick-account-access-text">Your Cart</div>
+        <div className="div-span"></div>
+        <div className="quick-account-access-text">Your Account</div>
+        <div className="div-span"></div>
+        <div className="quick-account-access-text">Your Lists</div>
       </div>
       </>
       }
