e9d88ec5f312f409cef1167259882534ab25e77b
EricJamesCrow <EricCrow@pm.me>
2023-03-19 18:30:21 -0700

    Update Cart.js

--- frontend/src/pages/new_cart/Cart.js
+++ frontend/src/pages/new_cart/Cart.js
@@ -22,18 +22,20 @@ export default function Cart() {
 
   return (
     <div className="cart-container">
-      <h1>My Cart</h1>
-      <div className="cart-products-container">
-      {cart.cartItems && cart.cartItems.map(item => (
-        <Order key={item._id} item={item} user={user}/>
-      ))}
+      <div className="login-wrapper">
+        <h1>My Cart</h1>
+        <div className="cart-products-container">
+        {cart.cartItems && cart.cartItems.map(item => (
+          <Order key={item._id} item={item} user={user}/>
+        ))}
+          </div>
+        <div className="subtotal-container">
+          <div>Subtotal:</div>
+          <div>{`$${subtotal}`}</div>
         </div>
-      <div className="subtotal-container">
-        <div>Subtotal:</div>
-        <div>{`$${subtotal}`}</div>
-      </div>
-      <div className="checkout-btn-container">
-        <Link to="/cart/checkout" type="button" className="add-to-cart-btn">Proceed to Checkout</Link>
+        <div className="checkout-btn-container">
+          <Link to="/cart/checkout" type="button" className="add-to-cart-btn">Proceed to Checkout</Link>
+        </div>
       </div>
       </div>
   )
