1ab4d976069193dd813e4010fdcca767a2a3d3d4
EricJamesCrow <EricCrow@pm.me>
2023-03-19 17:58:48 -0700

    updated change password css

--- frontend/src/pages/new_profile/pages/ChangePassword.js
+++ frontend/src/pages/new_profile/pages/ChangePassword.js
@@ -14,36 +14,38 @@ import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 export default function ChangePassword() {
     const navigate = useNavigate();
   return (
-    <div className="login-container admin-orders-details-page-container">
-    <button className="details-page-btn" onClick={() => navigate(-1)}>
-      <FontAwesomeIcon 
-        icon={faChevronLeft} 
-        style={{
-            color: "#BCBDBC",
-            fontSize: "1.15rem",
-            cursor: "pointer"}}
-        />
-    </button>
-    <form>
-    <div className="login-fields-container">
-      <h1>New Password</h1>
-      <div className="input-fields">
-        <div>Password<span className="required-asterisk">*</span></div>
-        <Input
-        type="password"
-        />
-      </div>
-      <div className="input-fields">
-        <div>Confirm Password<span className="required-asterisk">*</span></div>
-        <Input
-        type="password"
-        />
+      <div className="login-container admin-orders-details-page-container">
+      <button className="details-page-btn" onClick={() => navigate(-1)}>
+        <FontAwesomeIcon 
+          icon={faChevronLeft} 
+          style={{
+              color: "#BCBDBC",
+              fontSize: "1.15rem",
+              cursor: "pointer"}}
+          />
+      </button>
+      <div className="change-password-wrapper">
+        <form>
+        <div className="login-fields-container">
+          <h1>New Password</h1>
+          <div className="input-fields">
+            <div>Password<span className="required-asterisk">*</span></div>
+            <Input
+            type="password"
+            />
+          </div>
+          <div className="input-fields">
+            <div>Confirm Password<span className="required-asterisk">*</span></div>
+            <Input
+            type="password"
+            />
+          </div>
+        </div>
+        <div className="input-field-submit-container change-password">
+          <button>Done</button>
+        </div>
+        </form>
       </div>
     </div>
-    <div className="input-field-submit-container">
-      <button>Done</button>
-    </div>
-    </form>
-  </div>
   )
 }
