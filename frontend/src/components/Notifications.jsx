cbe8c1cc488d5f9543dbb3e5d017bf5abde01b4f
EricJamesCrow <EricCrow@pm.me>
2023-03-21 18:21:45 -0700

    Update Notifications.jsx

--- frontend/src/components/Notifications.jsx
+++ frontend/src/components/Notifications.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 // redux
@@ -7,16 +7,39 @@ import { useSelector } from 'react-redux';
 // chakra ui
 import { useToast } from '@chakra-ui/react'
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 export default function Notifications() {
     const navigate = useNavigate();
+    const cart = useSelector(state => state.cart);
+    const products = useSelector(state => state.products.products);
+    const cartItems = cart.cartItems;
     const addToCartTriggered = useSelector(state => state.notifications.addToCartTriggered);
     const removedItemTriggered = useSelector(state => state.notifications.removedItemTriggered);
     const toast = useToast();
 
+    const prevCartItems = usePrevious(cartItems);
+
     useEffect(() => {
         const showToast = () => {
+          if (!prevCartItems) return;
+
+          const updatedItem = cartItems.find((currentItem, index) => {
+            const prevItem = prevCartItems[index];
+            return !prevItem || prevItem.quantity !== currentItem.quantity;
+          });
+    
+          const productId = updatedItem ? updatedItem.product : cartItems[cartItems.length - 1].product;
+          const productName = products.find((product) => product._id === productId).name;
+
           toast({
-            title: "Item added to cart",
+            title: `${productName} added to cart`,
             description: (
                 <div onClick={() => {navigate("/cart")}}
                 style={{
