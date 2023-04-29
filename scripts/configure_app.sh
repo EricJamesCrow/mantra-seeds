4b65be6a4c9511b550fa458bf6308e3c2b66581c
EricJamesCrow <EricCrow@pm.me>
2023-04-29 13:47:34 -0700

    updating nginx.conf

--- scripts/configure_app.sh
+++ scripts/configure_app.sh
@@ -4,8 +4,8 @@
 # source /var/app/backend/.env
 
 # Set up Nginx configuration for serving the React app
-cp nginx.conf /etc/nginx/sites-available/my-app
-ln -s /etc/nginx/sites-available/my-app /etc/nginx/sites-enabled/
+cp nginx.conf /etc/nginx/conf.d/nginx.conf
+# ln -s /etc/nginx/sites-available/my-app /etc/nginx/sites-enabled/
 
 # Reload the Nginx configuration
 systemctl reload nginx
