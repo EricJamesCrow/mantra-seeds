aece5930b80360f59aa79530dea46066615a5d52
EricJamesCrow <EricCrow@pm.me>
2023-04-29 15:19:11 -0700

    troubleshooting

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Start the Node.js server with PM2, assuming the main entry point is named server.js
-cd /var/backend
-pm2 start server.js --name my-app-backend
+# cd /var/backend
+# pm2 start server.js --name my-app-backend
 
 # Restart Nginx to apply the configuration changes
 service nginx restart
