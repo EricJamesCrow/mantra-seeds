aa3c0bb83a1c34f91d0814e0323e3c3f5e9244dc
EricJamesCrow <EricCrow@pm.me>
2023-04-29 22:21:51 -0700

    update build spec

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -2,7 +2,7 @@
 
 # Start the Node.js server with PM2, assuming the main entry point is named server.js
 cd /var/backend
-pm2 start server.js --name my-app-backend
+npm run start
 
 # Restart Nginx to apply the configuration changes
 sudo service nginx restart
