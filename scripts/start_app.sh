7eeb2e3c0bace2c115805efc6537016e7ccc43d3
EricJamesCrow <EricCrow@pm.me>
2023-04-30 19:50:42 -0700

    update server.js

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -2,6 +2,7 @@
 
 # Start the Node.js server with PM2, assuming the main entry point is named server.js
 cd /var/backend
+# testing
 npm_prefix=$(npm config get prefix)
 export PATH=$PATH:${npm_prefix}/bin
 pm2 start server.js --name mantra-seeds-backend
