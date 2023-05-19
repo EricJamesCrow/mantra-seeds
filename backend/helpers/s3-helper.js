b4e5e39b57f13395c70198f1c953b8cdaec6eafe
EricJamesCrow <EricCrow@pm.me>
2023-05-18 17:27:01 -0700

    Update s3-helper.js

--- backend/helpers/s3-helper.js
+++ backend/helpers/s3-helper.js
@@ -4,25 +4,29 @@ const { v4: uuidv4 } = require('uuid');
 const s3 = new AWS.S3();
 
 async function uploadImage(image) {
-    const fileName = `${uuidv4()}.jpg`;
+    try {
+        const fileName = `${uuidv4()}.jpg`;
 
-    const params = {
-        Bucket: process.env.AWS_S3_BUCKET_NAME,
-        Key: fileName,
-        Body: image,
-        ContentType: 'image/jpeg'
-    };
-    
-    const result = await s3.upload(params).promise();
+        const params = {
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Key: fileName,
+            Body: image,
+            ContentType: 'image/jpeg'
+        };
+        
+        const result = await s3.upload(params).promise();
 
-    // replace S3 bucket URL with CloudFront URL
-    const cloudFrontDomain = process.env.CLOUDFRONT_URL;  // replace with your CloudFront domain
-    const cloudFrontUrl = result.Location.replace(`https://${process.env.AWS_S3_BUCKET_NAME}.s3.us-west-2.amazonaws.com`, cloudFrontDomain);
+        // replace S3 bucket URL with CloudFront URL
+        const cloudFrontDomain = process.env.CLOUDFRONT_URL;  // replace with your CloudFront domain
+        const cloudFrontUrl = result.Location.replace(`https://${process.env.AWS_S3_BUCKET_NAME}.s3.us-west-1.amazonaws.com`, cloudFrontDomain);
 
-    return cloudFrontUrl;
+        return cloudFrontUrl;
+    } catch (error) {
+        console.error('Error uploading image: ', error);
+        return null;  // Re-throw the error so it can be handled elsewhere if needed
+    }
 };
-
 
 module.exports = {
     uploadImage,
-  };
+};
