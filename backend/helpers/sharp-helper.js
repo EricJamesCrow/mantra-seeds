289155b9f7819e317d79e737c90787628a70710c
EricJamesCrow <EricCrow@pm.me>
2023-06-03 18:22:38 -0700

    Update sharp-helper.js

--- backend/helpers/sharp-helper.js
+++ backend/helpers/sharp-helper.js
@@ -2,11 +2,22 @@ const sharp = require('sharp');
 
 const WIDTH = 420;
 const HEIGHT = 420;
+const MAX_SIZE = 1024 * 1024;  // 1MB
 
 // Resize image
 const resizeImage = async (image) => { 
     try {
-      const outputBuffer = await sharp(image.buffer)
+      let inputBuffer = image.buffer;
+    
+      // Check if image size is greater than MAX_SIZE
+      if (inputBuffer.byteLength > MAX_SIZE) {
+        // Compress the image if size > MAX_SIZE
+        inputBuffer = await sharp(inputBuffer)
+          .jpeg({ quality: 50 })  // You can adjust the quality value to fit your needs
+          .toBuffer();
+      }
+
+      const outputBuffer = await sharp(inputBuffer)
         .resize(WIDTH, HEIGHT, {
           fit: 'cover',
           position: 'center',
