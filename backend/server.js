79a9501e81a94ba64ab26fc41e46a45190f6472e
EricJamesCrow <EricCrow@pm.me>
2023-06-09 20:12:14 -0700

    dynamically creates sitemap.xml

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,9 @@ const paypalRoutes = require('./routes/payment/paypal')
 const contactRoutes = require('./routes/contact')
 const recipientRoutes = require('./routes/recipient')
 
+// products for sitemap.xml generation
+const Product = require('./models/productModel')
+
 // inventory
 const { releaseReservedItems } = require('./utilities/inventoryUtils')
 
@@ -51,6 +54,50 @@ app.use('/api/payment', paymentRoutes)
 // app.use('/api/payment/stripe', stripeRoutes)
 app.use('/api/payment/paypal', paypalRoutes)
 app.use('/api/recipient', recipientRoutes)
+
+app.get('/sitemap.xml', async (req, res) => {
+    try {
+        let xml = '<?xml version="1.0" encoding="UTF-8"?>';
+        xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+        const products = await Product.find({deleted: {$ne: true}}).sort({ createdAt: -1 });
+
+        const staticPages = [
+            { url: 'https://www.mantra-seeds.com/', changefreq: 'daily', priority: 1.0 },
+            { url: 'https://www.mantra-seeds.com/shop', changefreq: 'daily', priority: 0.8 },
+            { url: 'https://www.mantra-seeds.com/contact', changefreq: 'monthly', priority: 0.8 },
+            { url: 'https://www.mantra-seeds.com/login', changefreq: 'monthly', priority: 0.8 },
+            { url: 'https://www.mantra-seeds.com/signup', changefreq: 'monthly', priority: 0.8 },
+            { url: 'https://www.mantra-seeds.com/terms-and-conditions', changefreq: 'yearly', priority: 0.5 },
+            { url: 'https://www.mantra-seeds.com/privacy-policy', changefreq: 'yearly', priority: 0.5 },
+          ];
+        
+          staticPages.forEach(page => {
+            xml += '<url>';
+            xml += `<loc>${page.url}</loc>`;
+            xml += '<lastmod>' + new Date().toISOString() + '</lastmod>';
+            xml += `<changefreq>${page.changefreq}</changefreq>`;
+            xml += `<priority>${page.priority}</priority>`;
+            xml += '</url>';
+          });
+      
+        for(let product of products){
+          xml += '<url>';
+          xml += `<loc>https://www.mantra-seeds.com/shop/products/${product.id}</loc>`;
+          xml += '<lastmod>' + new Date().toISOString() + '</lastmod>';
+          xml += '<changefreq>weekly</changefreq>';
+          xml += '<priority>0.8</priority>';
+          xml += '</url>';
+        }
+      
+        xml += '</urlset>';
+        
+        res.header('Content-Type', 'text/xml');
+        res.send(xml);
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 // stripe public key
 app.get("/config", (req, res) => {
